74b590ec721c89d645b3a0b3c9e93296b0c67151
Shoma <shoma.gujjar@gmail.com>
2022-06-24 14:22:05 +0100

    initial commit

---
+++ ConvertToRomanNumeralsTest/ConvertToRomanNumeralsTest.cs
@@ -0,0 +1,29 @@
+using ConvertToRomanNumerals;
+
+namespace ConvertToRomanNumeralsTest
+{
+    public class RomanNumeralsTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase(123, "CXXIII")]
+        [TestCase(2000, "MM")]
+        [TestCase(1, "I")]
+        [TestCase(1987, "MCMLXXXVII")]
+        [TestCase(4999, "MMMMCMXCIX")]
+        [TestCase(1645, "MDCXLV")]
+        [TestCase(5, "V")]
+        public void ConvertNumbersRomanNumerals_ReturnValidRomanNumerals(int input, string expectedResult)
+        {
+            //Arrange
+            //Act
+            var actualResult = ToRomanNumerals.ConvertNumbersRomanNumerals(input);
+            //Assert
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+    }
+}
